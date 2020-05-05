0cb32dc8b7a9a16197c6e9b54d83baa9003d3977
Fandeqing <32571196+Fandeqing@users.noreply.github.com>
2020-05-05 15:15:54 +0800

    Updates

---
+++ precache-manifest.900a5e9b8c6c5854d7ce013e4a5a9822.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee20367b8635080059c8b694ec1236b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "437ef3352c3e70211d7f",
+    "url": "/monsters-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monsters-rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "437ef3352c3e70211d7f",
+    "url": "/monsters-rolodex/static/js/main.eaa594da.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
